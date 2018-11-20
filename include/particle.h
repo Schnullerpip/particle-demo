6303406b04e62d33cd16f3258f16d0d12af5ef04
julian <schnullerpip@gmail.com>
2018-11-20 09:54:42 +0100

    moved some stuff out of h file but intellisense not working...

--- include/particle.h
+++ include/particle.h
@@ -45,102 +45,32 @@ struct particle
     }
 };
 
-template<unsigned int size>
 struct particle_system
 {
     particle *m_prototype;
-    float m_rate;
+    size_t m_size;
     size_t m_bulk;
+    float m_rate;
     glm::vec3 (*m_get_spawn_position)();
     glm::vec3 (*m_get_initial_acceleration)();
     glm::vec3 (*m_get_initial_velocity)();
 
-    particle particles[size];
+    particle *particles;
     //3f position, 1f life
-    float particles_data[size*(3/*xyz*/+1/*life*/)];
+    float *particles_data; //[size*(3/*xyz*/+1/*life*/)];
 
-    particle_system(particle *prototype, float rate_in_seconds, size_t bulk, glm::vec3 (*spawn_position)(), glm::vec3 (*acc_func_ptr)(), glm::vec3 (*vel_func_ptr)())
-        :m_prototype(prototype),
-        m_rate(rate_in_seconds),
-        m_bulk(bulk),
-        m_get_spawn_position(spawn_position),
-        m_get_initial_acceleration(acc_func_ptr),
-        m_get_initial_velocity(vel_func_ptr)
-    {
-        for(size_t i = 0; i < size; ++i)
-        {
-            reset_particle<dead>(&particles[i]);
-        }
+    particle_system(
+        particle *prototype,
+        float rate_in_seconds,
+        size_t bulk,
+        glm::vec3 (*spawn_position)(),
+        glm::vec3 (*acc_func_ptr)(),
+        glm::vec3 (*vel_func_ptr)());
 
-        glGenVertexArrays(1, &vao);
-        glGenBuffers(1, &vbo);
 
-        glBindVertexArray(vao);
+    void update_particles(glm::vec3 camera_position);
 
-        glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * size, particles_data, GL_DYNAMIC_DRAW);
-
-        GLsizei stride = 4 * sizeof(GLfloat);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, stride, (void*)(sizeof(GLfloat)*3));
-        glEnableVertexAttribArray(1);
-    }
-
-    unsigned int particles_alive = 0;
-    void update_particles(glm::vec3 camera_position)
-    {
-        static float time_count = 0;
-        float delta_time = jule::delta_time;
-        particles_alive = 0;
-
-        if((time_count += delta_time) >= m_rate)
-        {
-            time_count = 0;
-            for(size_t i = 0; i < m_bulk; ++i)
-            {
-                reset_first_unused();
-            }
-        }
-
-        for(size_t s = 0; s < size; ++s)
-        {
-            particle &p = particles[s];
-
-            p.update_position();
-
-            if((p.m_life -= delta_time) > 0.f)
-            {
-                ++particles_alive;
-            }
-        }
-
-        //sort out dead particles
-        std::sort(particles, particles+size, [](particle &p1, particle &p2){
-            return p1.m_life > p2.m_life;});
-
-        //sort alive particles in correct order for camera (so blending is right)
-        std::sort(particles, particles+particles_alive, [&](particle &p1, particle &p2){
-            return  glm::length2(camera_position-p1.m_xyz) > glm::length2(camera_position-p2.m_xyz);
-        });
-
-        for(size_t s = 0; s < particles_alive; ++s)
-        {
-            particle &p = particles[s];
-
-            size_t offset = s * 4;
-            particles_data[offset + x] = p.m_xyz.x;
-            particles_data[offset + y] = p.m_xyz.y;
-            particles_data[offset + z] = p.m_xyz.z;
-            particles_data[offset + life] = p.m_life;
-        }
-
-        glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        //glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * particles_alive, particles_data + num_dead, GL_DYNAMIC_DRAW);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 4 * particles_alive, particles_data, GL_DYNAMIC_DRAW);
-    }
-
-    void render_particles()
+    inline void render_particles()
     {
         glDrawArrays(GL_POINTS, 0, particles_alive);
     }
@@ -158,36 +88,15 @@ struct particle_system
         p->m_vel = m_get_initial_velocity();
     }
 
-    void reset_first_unused()
-    {
-        static unsigned int last_found = 0;
-        for(unsigned int i = last_found; i < size; ++i)
-        {
-            particle &p = particles[i];
-            if((p.m_immortal == NO) && (p.m_life < 0.f))
-            {
-                last_found = i;
-                reset_particle<alive>(&p);
-                return;
-            }
-        }
-        for(unsigned int i = 0; i < last_found; ++i)
-        {
-            particle &p = particles[i];
-            if((p.m_immortal == NO) && (p.m_life < 0.f))
-            {
-                last_found = i;
-                reset_particle<alive>(&p);
-                return;
-            }
-        }
-    }
+    void reset_first_unused();
     
-    void use()
+    inline void use()
     {
         glBindVertexArray(vao);
     }
+
 private:
+    unsigned int particles_alive = 0;
     unsigned int vao;
     unsigned int vbo;
 };
